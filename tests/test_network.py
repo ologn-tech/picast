8c9aa43714bd446cc360719249e13a358016915c
Hiroshi Miura <miurahr@linux.com>
2019-11-02 22:22:41 +0900

    test: update for asyncio
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,10 +1,11 @@
-
+import asyncio
 import socket
 import threading
 
 import pytest
 
-from picast.rtspserver import RtspServer
+from picast.rtspsink import RtspSink
+from picast.video import RasberryPiVideo
 
 
 class MockServer(threading.Thread):
@@ -21,49 +22,21 @@ class MockServer(threading.Thread):
         conn.close()
 
 
-@pytest.mark.unit
-def test_connection():
-    s = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
-    s.bind(('localhost', 0))
-    address, port = s.getsockname()
-    s.close()
+@pytest.mark.asyncio
+async def test_open_connection(monkeypatch, unused_tcp_port):
 
-    server = MockServer(port)
+    def videomock(self):
+        return "06 00 01 10 000101C3 00208006 00000000 00 0000 0000 00 none none"
+
+    def nonemock(self, *args):
+        return
+
+    monkeypatch.setattr(RasberryPiVideo, "get_wfd_video_formats", videomock)
+    monkeypatch.setattr(RasberryPiVideo, "_get_display_resolutions", nonemock)
+
+    server = MockServer(unused_tcp_port)
     server.start()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    assert RtspServer._connect(sock, '127.0.0.1', port)
-    sock.close()
+    player = None
+    rtspserver = RtspSink(player)
+    assert await rtspserver.open_connection('127.0.0.1', unused_tcp_port)
     server.join()
-
-
-@pytest.mark.unit
-def test_parse_header_200_OK():
-    data = "RTSP/1.0 200 OK\r\nCSeq: 1\r\n\r\n"
-    headers, body = RtspServer._split_header_body(data)
-    cmd, url, resp, seq, others = RtspServer._rtsp_parse_headers(headers)
-    assert cmd is None
-    assert url is None
-    assert resp == "200 OK"
-    assert seq == 1
-
-
-@pytest.mark.unit
-def test_parse_rtsp_parse_header_options():
-    data = 'OPTIONS * RTSP/1.0\r\nCSeq: 2\r\n\r\n'
-    headers, body = RtspServer._split_header_body(data)
-    cmd, url, resp, seq, others = RtspServer._rtsp_parse_headers(headers)
-    assert cmd == 'OPTIONS'
-    assert url == '*'
-    assert resp is None
-    assert seq == 2
-
-
-@pytest.mark.unit
-def test_parse_rtsp_parse_header_setup():
-    data = 'SETUP rtsp://192.168.173.80/wfd1.0/streamid=0 RTSP/1.0\r\nCSeq: 3\r\n\r\n'
-    headers, body = RtspServer._split_header_body(data)
-    cmd, url, resp, seq, others = RtspServer._rtsp_parse_headers(headers)
-    assert cmd == 'SETUP'
-    assert url == 'rtsp://192.168.173.80/wfd1.0/streamid=0'
-    assert resp is None
-    assert seq == 3
