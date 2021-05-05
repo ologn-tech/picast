5db0cf9f8e5c26db5d336bf1a574fcc103f4fdd0
Anton Panikov <Anton Panikov anton.panikov+github@gmail.com>
2021-05-05 01:37:31 -0400

    Added player tests

---
+++ tests/test_player.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+import tempfile
+
+import pytest
+
+from picast.player import NopPlayer, VlcPlayer
+
+class ProcessMock:
+
+    def __init__(self, args, expect_arg):
+        self.terminate_count = 0
+        assert args[0] == expect_arg
+
+    def terminate(self):
+        self.terminate_count += 1
+
+@pytest.mark.unit
+def test_nop_player_start_stop(monkeypatch):
+    def mock_proc(args):
+        return ProcessMock(args, 'echo')
+
+    monkeypatch.setattr(subprocess, 'Popen', mock_proc)
+
+    player = NopPlayer()
+    player.start()
+    player.stop()
+    assert player.proc.terminate_count == 1
+
+@pytest.mark.unit
+def test_vlc_player_start_stop(monkeypatch):
+    def mock_proc(args):
+        return ProcessMock(args, 'cvlc')
+
+    monkeypatch.setattr(subprocess, 'Popen', mock_proc)
+
+    player = VlcPlayer()
+    player.start()
+    player.stop()
+    assert player.vlc.terminate_count == 1
